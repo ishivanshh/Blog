926d4daabf9131d8d0469d2998a0f6ce6152c498
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-08-27 23:54:02 +0530

    fix for vercel

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-
 import { Link, useNavigate } from "react-router-dom";
-import { getProfile, logoutUser } from "../services/authService";
+import { getProfile, logoutUser } from "../services/authService.js"
 import { getMyBlogs } from "../services/blogService";
 
 
