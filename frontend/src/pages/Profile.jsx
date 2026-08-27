18273bfc3cd2a98ee22e243aa443dda8731c41c9
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-08-28 00:08:44 +0530

    vercel errors...

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getProfile, logoutUser } from "../services/authService.js"
+import { getProfile, logoutUser } from "../services/authService"
 import { getMyBlogs } from "../services/blogService";
 
 
